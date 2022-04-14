4a4286ce396983984accfcbafebca7c431328e49
csh4430 <snghun6889@gmail.com>
2022-04-14 20:13:19 +0900

    와 AI

--- src/client/Assets/01.Scripts/01.Core/FSM.cs
+++ src/client/Assets/01.Scripts/01.Core/FSM.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,27 +7,52 @@ using UnityEngine.Events;
 public enum State
 {
     Idle,
+    Chase,
 }
 
-public class FSM : MonoBehaviour
+[Serializable]
+public class FSM
 {
-    private State _currentState = State.Idle;
-    private Dictionary<State, UnityEvent> _stateDictionary;
-    private Dictionary<State, bool> _conditionDictionary;
-
+    public State _currentState = State.Idle;
+    private Dictionary<State, UnityEvent> _eventDictionary = new Dictionary<State, UnityEvent>();
+    public FSM()
+    {
+        _currentState = State.Idle;
+    }
     public FSM(State init = State.Idle)
     {
         _currentState = init;
     }
 
-    private void Update()
+    public void UpdateInvoke()
     {
-        if (_conditionDictionary[_currentState])
+        if(_eventDictionary.ContainsKey(_currentState))
+            _eventDictionary[_currentState]?.Invoke();
+        else
         {
-            _stateDictionary[_currentState]?.Invoke();
-
+            _eventDictionary.Add(_currentState, null);
+            return;
         }
     }
 
+    public State ChangeState(State state)
+    {
+        _currentState = state;
+        return _currentState;
+    }
 
+    public void AddActionOnState(State state, UnityAction action)
+    {
+        if (!_eventDictionary.ContainsKey(state))
+        {
+            _eventDictionary.Add(state, new UnityEvent());
+        }
+        if(action != null)
+            _eventDictionary[state].AddListener(action);
+    }
+
+    public void RemoveActionOnState(State state, UnityAction action)
+    {
+        _eventDictionary[state].RemoveListener(action);
+    }
 }
--- src/client/Assets/01.Scripts/03.Bases/AI/EnemyAI.cs
+++ src/client/Assets/01.Scripts/03.Bases/AI/EnemyAI.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyAI : MonoBehaviour
 {
+    [SerializeField] private FSM _thisFSM = new FSM();
+    [SerializeField] private UnityEvent<Vector2> OnMoveSignal;
+    [SerializeField] private UnityEvent OnStopSignal;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
+    {
+        _thisFSM.AddActionOnState(State.Idle, StopAgent);
+        _thisFSM.AddActionOnState(State.Chase, MoveAgent);
+    }
+    private void MoveAgent()
+    {
+        OnMoveSignal?.Invoke(
+            (Physics2D.OverlapCircle(transform.position, 5,
+            LayerMask.GetMask("Player")).transform.position - transform.position)
+            .normalized);
+    }
+    private void StopAgent()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Physics2D.OverlapCircle(transform.position, 5, LayerMask.GetMask("Player")) != null)
+        {
+            _thisFSM.ChangeState(State.Chase);
+        }
+        else
+        {
+            _thisFSM.ChangeState(State.Idle);
+
+        }
+        _thisFSM.UpdateInvoke();
     }
 }
