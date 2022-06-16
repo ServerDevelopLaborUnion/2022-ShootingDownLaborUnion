3a73a93f8e0036a05720df620d6e26b48c0f252d
paul050210 <leepaul050210@gmail.com>
2022-06-16 20:07:58 +0900

    PhaseData

---
+++ src/client/Assets/01.Scripts/TestCodes/PhaseData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpawnType
+{
+    circle,
+    width,
+    length
+}
+[Serializable]
+public class PhaseData
+{
+    public int maxEnemy;
+    public SpawnType spawnType;
+    public PhaseData(int maxEnemy, SpawnType spawnType)
+    {
+        this.maxEnemy = maxEnemy;
+        this.spawnType = spawnType;
+    }
+}
--- src/client/Assets/01.Scripts/TestCodes/TestEnemySpawn.cs
+++ src/client/Assets/01.Scripts/TestCodes/TestEnemySpawn.cs
@@ -10,21 +10,52 @@ public class TestEnemySpawn : MonoBehaviour
     private GameObject Enemey;
     [SerializeField]
     private float spawnDelay = 3f;
-
     [SerializeField]
     private float radius = 7f;
+    [SerializeField]
+    private List<PhaseData> phases = new List<PhaseData>();
 
     private void Start()
     {
-        StartCoroutine("SpawnEnemey");
+        StartCoroutine(Spawn());
+    }
+
+    private void SetPhase()
+    {
+        PhaseData phase = new PhaseData(40, SpawnType.circle);
+        phases.Add(phase);
+    }
+
+    private IEnumerator Spawn()
+    {
+        for(int i =0; i<phases.Count; i++)
+        {
+            switch(phases[i].spawnType)
+            {
+                case SpawnType.circle:
+                    yield return StartCoroutine(CircleSpawn(phases[i]));
+                    break;
+                case SpawnType.width:
+                    yield return StartCoroutine(WidthSpawn(phases[i]));
+                    break;
+                case SpawnType.length:
+                    yield return StartCoroutine(LengthSpawn(phases[i]));
+                    break;
+            }
+
+            yield return new WaitForSeconds(1000f);
+        }
     }
     
-    private IEnumerator SpawnEnemey()
+    private IEnumerator CircleSpawn(PhaseData phaseData)
     {
+        int enemyCnt = 0;
         while(true)
         {
             for (float angle = 0; angle < 360; angle += 10)
             {
+                if (enemyCnt >= phaseData.maxEnemy) yield break;
+                enemyCnt++;
                 float x = Mathf.Cos(Mathf.Deg2Rad * angle) * radius + TargetPlayer.transform.position.x;
                 float y = Mathf.Sin(Mathf.Deg2Rad * angle) * radius + TargetPlayer.transform.position.y;
                 Vector2 enemyPos = new Vector2(x, y);
@@ -34,5 +65,15 @@ public class TestEnemySpawn : MonoBehaviour
                 yield return new WaitForSeconds(spawnDelay);
             }
         }
+    }
+
+    private IEnumerator WidthSpawn(PhaseData phaseData)
+    {
+        yield return null;
+    }
+
+    private IEnumerator LengthSpawn(PhaseData phaseData)
+    {
+        yield return null;
     }
 }
