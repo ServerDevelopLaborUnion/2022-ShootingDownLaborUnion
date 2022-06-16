6c716829800807a21f810ace61b3b13efa1766ff
paul050210 <leepaul050210@gmail.com>
2022-06-16 19:07:07 +0900

    useEntity

--- src/client/Assets/01.Scripts/TestCodes/TestEnemySpawn.cs
+++ src/client/Assets/01.Scripts/TestCodes/TestEnemySpawn.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,18 +18,19 @@ public class TestEnemySpawn : MonoBehaviour
     {
         StartCoroutine("SpawnEnemey");
     }
-
+    
     private IEnumerator SpawnEnemey()
     {
         while(true)
         {
-            
             for (float angle = 0; angle < 360; angle += 10)
             {
                 float x = Mathf.Cos(Mathf.Deg2Rad * angle) * radius + TargetPlayer.transform.position.x;
                 float y = Mathf.Sin(Mathf.Deg2Rad * angle) * radius + TargetPlayer.transform.position.y;
-                Instantiate(Enemey);
-                Enemey.transform.position = new Vector2(x, y);
+                Vector2 enemyPos = new Vector2(x, y);
+                // TODO : uuid, owner설정
+                EntityData entityData = new EntityData("uuid", "owner", "name", enemyPos, Quaternion.Euler(0f, 0f, 0f), EntityType.Enemy);
+                Entity.EntityCreate(entityData);
                 yield return new WaitForSeconds(spawnDelay);
             }
         }
