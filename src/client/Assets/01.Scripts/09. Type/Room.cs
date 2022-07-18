8f04c3172f2ca74c6398c8791a343dca58b15ee0
KBluePurple <42506968+BlWon@users.noreply.github.com>
2022-07-18 18:45:25 +0900

    RoomManager 구조

--- src/client/Assets/01.Scripts/09. Type/Room.cs
+++ src/client/Assets/01.Scripts/09. Type/Room.cs
@@ -4,4 +4,14 @@ public class Room
 {
     public RoomInfo Info { get; private set; }
     public List<User> Players { get; private set; }
+
+    public void AddUser(User user)
+    {
+        Players.Add(user);
+    }
+
+    public void ExitUser(User user)
+    {
+        Players.Remove(user);
+    }
 }
