0be9c942ccbf575e45d3d631c5a8f2628e41d2e2
Bwasabe <jixg20@gmail.com>
2022-07-23 01:23:06 +0900

    RoomUI, ColorHierarchy

--- src/client/Assets/01.Scripts/00.Scenes/03.Room/00.Managers/RoomManager.cs
+++ src/client/Assets/01.Scripts/00.Scenes/03.Room/00.Managers/RoomManager.cs
@@ -1,29 +1,23 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class RoomManager : MonoSingleton<RoomManager>
 {
     [SerializeField]
-    private TMP_Text _userCountText;
+    private List<RolePanel> _rolePanels = new List<RolePanel>();
 
-    [SerializeField]
-    private TMP_Text[] _userNameTexts;
 
     private void Start()
     {
         // UI 에 CurrentRoom 정보 업데이트
-        for (int i = 0; i < Storage.CurrentRoom.Users.Count; ++i){
-            _userNameTexts[i].text = Storage.CurrentRoom.Users[i].Name;
-        }
-        
     }
 
     public void OnUserJoin(User user)
     {
         //  UI 인원수 업데이트
         // 플레이어 닉네임 가져와서 UI업데이트
-        _userNameTexts[Storage.CurrentRoom.Users.Count].text = user.Name;
-        _userCountText.text = $"{Storage.CurrentRoom.Users} / {RoomInfo.MaxPlayers}";
+        //_userNameTexts[Storage.CurrentRoom.Users.Count].text = user.Name;
     }
 
     public void OnUserLeave(User user)
@@ -34,61 +28,56 @@ public class RoomManager : MonoSingleton<RoomManager>
 
     }
 
-    public void OnReadyStateUpdate(User user){
-        if(user.IsReady){
+    public void OnUpdateRole(User user, int role, bool isReady)
+    {
+        user.IsReady = isReady;
+        _rolePanels[role].ActiveReadyPanel(user.IsReady);
+
+
+        if (user.IsReady)
+        {
+            _rolePanels[role].SetNameText(Storage.CurrentUser.Name);
             //TODO: 대충 인원수 UI++
-            if(Storage.CurrentRoom.Users.Count == RoomInfo.MaxPlayers && user.IsMaster){
+            if ( CheckAllUserIsReady() && Storage.CurrentUser.IsMaster)
+            {
                 // TODO: 시작 버튼 생김
+                _rolePanels[role].ActiveStartBtn(true);
             }
         }
-        else{
-            //TODO: 대충 인원수 UI--
+        else
+        {
+            _rolePanels[role].SetNameText(string.Empty);
         }
-
     }
 
-    public void OnSelectRole(User user){
-        // TODO: 대충 직업 UI 바꾸기
-    }
-
-    public void SetReady(bool isReady)
+    public void SetRole(int role, bool isReady)
     {
-        //레디를 눌렀을 때
-        WebSocket.Client.SetReady(isReady);
-    }
-
-    public void SetRole(int role)
-    {
-        //무기를 눌렀을 때
+        // 무기를 눌렀을 때
         // 무기를 바꿨을 때 무기가 겹칠경우 실행이 안되게
-        WebSocket.Client.SetRole(role);
+        //WebSocket.Client.SetRole(role);
     }
 
-    public void OnStartGame(){
+    public void ClickStartGame(){
+        //TODO: 씬 로딩
+        //WebSocket.Client.ClickStartGame;
+    }
+
+    public void OnStartGame()
+    {
         // 게임 시작 버튼을 눌렀을 때
         // 인원수가 맞지 않거나, 겹치는 무기가 있을 경우 불가 메세지 출력
         // 방장이 아니면 못누름
-        if(IsOverlapJob()){
-            // TODO: 대충 직업이 겹쳐서 실행 못한다는 UI
-            return;
-        }
-        if(Storage.CurrentRoom.Users.Count < RoomInfo.MaxPlayers){
-            // TODO: 대충 인원수가 부족하다는 UI
-            return;
-        }
-
-        
     }
 
-    private bool IsOverlapJob(){
+    
+    private bool CheckAllUserIsReady(){
         for (int i = 0; i < Storage.CurrentRoom.Users.Count; ++i){
-            for (int j = 0; j < Storage.CurrentRoom.Users.Count; ++j){
-                if(i==j)continue;
-                if (Storage.CurrentRoom.Users[i].Role == Storage.CurrentRoom.Users[j].Role){
-                    return true;
-                }
+            if(!Storage.CurrentRoom.Users[i].IsReady){
+                return false;
             }
         }
-        return false;
+        return true;
     }
+
+
 }
--- src/client/Assets/01.Scripts/00.Scenes/03.Room/01.UI/RolePanel.cs
+++ src/client/Assets/01.Scripts/00.Scenes/03.Room/01.UI/RolePanel.cs
@@ -1,21 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 public class RolePanel : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _rolePanel;
+    private GameObject _readyPanel;
+    [SerializeField]
+    private GameObject _startBtn;
+
+    [SerializeField]
+    private TMP_Text _nameText;
 
     [SerializeField]
     private int _roleNumber;
-    public void OnClickReady(){
-        _rolePanel.SetActive(true);
-        RoomManager.Instance.SetRole(_roleNumber);
+
+    [SerializeField]
+    private float _duration = 4f;
+    [SerializeField]
+    private float _strength = 8f;
+    [SerializeField]
+    private int _vivrato = 10;
+    [SerializeField]
+    private float _randomness = 90;
+
+    private Tweener _startTextShakeTween;
+    public void ActiveReadyPanel(bool isActive)
+    {
+        _readyPanel.SetActive(isActive);
+
     }
 
-    public void OnClickCancel(){
-        _rolePanel.SetActive(false);
-        RoomManager.Instance.SetCancel(_roleNumber);
+    public void SetNameText(string name)
+    {
+        _nameText.text = name;
     }
+
+
+    public void OnClickReadyOrCancel(bool isReady)
+    {
+        RoomManager.Instance.SetRole(_roleNumber, isReady);
+    }
+
+    public void OnClickStartGame()
+    {
+
+        RoomManager.Instance.ClickStartGame();
+    }
+
+    public void ActiveStartBtn(bool isActive)
+    {
+        _startBtn.SetActive(isActive);
+        if (isActive)
+        {
+            _startTextShakeTween = _startBtn.transform.GetChild(0).DOShakePosition(_duration, _strength, _vivrato, _randomness).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        }
+        else
+        {
+            if(_startTextShakeTween != null){
+                _startTextShakeTween.Kill();
+            }
+         }
+    }
+
+
 }
